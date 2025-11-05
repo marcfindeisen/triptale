00cfee34490b4c6c4426a22da1bb31374eb05d33
Marc Findeisen <marc.findeisen@freenet.de>
2025-11-05 21:16:28 +0100

    feat: add splashscreen and PWA setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App.jsx"
-import "./styles.css"
-import { registerSW } from "virtual:pwa-register"
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './styles.css'
 
-registerSW({ immediate: true })
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+// Splash sanft ausblenden, wenn React geladen hat
+window.requestAnimationFrame(() => {
+  const el = document.getElementById('splash')
+  if (el) setTimeout(() => el.classList.add('hide'), 300)
+})
